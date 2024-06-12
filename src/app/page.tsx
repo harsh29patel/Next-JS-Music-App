edcdfece4db80c24bece21821eaad38541527ef3
harsh29patel <harshpatel290703@gmail.com>
2024-06-12 21:38:59 +0530

    featured courses

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import HeroSection from "./components/HeroSection";
+import FeautredCourses from "./components/FeautredCourses";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
        <h1 className="text-2xl  text-center"></h1>
        <HeroSection/>
+       <FeautredCourses/>
   </main>
   );
 }
