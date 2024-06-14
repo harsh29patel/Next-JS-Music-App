4437e4ffba1f60069487f2db2f4822f5a3b83c63
harsh29patel <harshpatel290703@gmail.com>
2024-06-14 15:00:32 +0530

    Contact us , Linkig  , All courses

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -53,9 +53,11 @@ const config: Config = {
   theme: {
     extend: {
        animation: {
+        "meteor-effect": "meteor 5s linear infinite",
         spotlight: "spotlight 2s ease .75s 1 forwards",
            scroll:
           "scroll var(--animation-duration, 40s) var(--animation-direction, forwards) linear infinite",
+          
       },
       keyframes: {
         spotlight: {
@@ -73,6 +75,14 @@ const config: Config = {
             transform: "translate(calc(-50% - 0.5rem))",
           },
         },
+      },
+      meteor:{
+        "0%": { transform: "rotate(215deg) translateX(0)", opacity: "1" },
+        "70%": { opacity: "1" },
+        "100%": {
+          transform: "rotate(215deg) translateX(-500px)",
+          opacity: "0",
+        }
       },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
